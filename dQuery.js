17eb394607a95285b34ce08cf0256026f63a0add
SkyyySi <rasti.b@protonmail.com>
2022-03-23 21:34:41 +0100

    first commit

---
+++ dQuery.js
@@ -0,0 +1,66 @@
+//@ts-check
+"use strict";
+
+export class dQuery {
+	/**
+	 * @type {any}
+	 */
+	doc = [];
+
+	/**
+	 * @param {string} element
+	 **/
+	constructor(element) {
+		this.doc = document.querySelectorAll(element);
+		return this;
+	}
+
+	first = () => {
+		this.doc = this.doc[0];
+		return this;
+	};
+
+	/**
+	 * @param {string} html
+	 **/
+	html = (html) => {
+		this.doc.innerHTML = html;
+		return this;
+	};
+
+	/**
+	 * @param {Array<any>} list
+	 * @param {Function} callback
+	 **/
+	foreach = (list, callback) => {
+		for (let i = 0; i < list.length - 1; i++) {
+			callback(list[i]);
+		}
+		return this;
+	};
+
+	/**
+	 * @param {string} paragraph
+	 **/
+	paragraph = (paragraph) => {
+		this.doc.innerHTML = `<p>${paragraph}</p>`;
+		return this;
+	};
+
+	/**
+	 * @param {string} element
+	 **/
+	appendToList = (element) => {
+		var newElement = document.createElement("li");
+		newElement.appendChild(document.createTextNode(element));
+		this.doc.appendChild(newElement);
+		return this;
+	}
+
+	/**
+	 * @param {Function} callback
+	 **/
+	afterPageLoad = (callback) => {
+		document.addEventListener("DOMContentLoaded", () => callback(/** @type {dQuery} */ this));
+	}
+}
